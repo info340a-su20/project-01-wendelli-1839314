220a3a287b1da323df125b33cbe8410dc084302c
georgeglye <georgegu@uw.edu>
2020-08-17 15:06:19 -0700

    Update html style for about page

--- js/script.js
+++ js/script.js
@@ -1,48 +1,48 @@
-// 'use strict';
+'use strict';
 
-// // import React from 'react';
-// // import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-// // view on load
-// window.onload = function() {
-//   displayHousekeeping();
-// }
+// view on load
+window.onload = function() {
+  displayHousekeeping();
+}
 
-// // activity center Menu Bar
-// var housekeepingActivity = document.getElementsByClassName("housekeeping");
-// var jobActivity = document.getElementsByClassName("job");
-// var transportationActivity = document.getElementsByClassName("transportation");
-// var entertainmentActivity = document.getElementsByClassName("entertainment");
-// var miscellaneousActivity = document.getElementsByClassName("miscellaneous");
+// activity center Menu Bar
+var housekeepingActivity = document.getElementsByClassName("housekeeping");
+var jobActivity = document.getElementsByClassName("job");
+var transportationActivity = document.getElementsByClassName("transportation");
+var entertainmentActivity = document.getElementsByClassName("entertainment");
+var miscellaneousActivity = document.getElementsByClassName("miscellaneous");
 
-// function showOrHide(category, showOrHide) {
-//   for (let i = 0; i < category.length; i++) {
-//     if (showOrHide) {
-//       category[i].style.display = "block";
-//     } else {
-//       category[i].style.display = "none";
-//     }
-//   }
-// }
+function showOrHide(category, showOrHide) {
+  for (let i = 0; i < category.length; i++) {
+    if (showOrHide) {
+      category[i].style.display = "block";
+    } else {
+      category[i].style.display = "none";
+    }
+  }
+}
 
-// function hideAllCategory() {
-//   showOrHide(housekeepingActivity, false);
-//   showOrHide(jobActivity, false);
-//   showOrHide(transportationActivity, false);
-//   showOrHide(entertainmentActivity, false);
-//   showOrHide(miscellaneousActivity, false);
-//   document.getElementById("option1").style.backgroundColor = "#475a7b";
-//   document.getElementById("option2").style.backgroundColor = "#475a7b";
-//   document.getElementById("option3").style.backgroundColor = "#475a7b";
-//   document.getElementById("option4").style.backgroundColor = "#475a7b";
-//   document.getElementById("option5").style.backgroundColor = "#475a7b";
-// }
+function hideAllCategory() {
+  showOrHide(housekeepingActivity, false);
+  showOrHide(jobActivity, false);
+  showOrHide(transportationActivity, false);
+  showOrHide(entertainmentActivity, false);
+  showOrHide(miscellaneousActivity, false);
+  document.getElementById("option1").style.backgroundColor = "#475a7b";
+  document.getElementById("option2").style.backgroundColor = "#475a7b";
+  document.getElementById("option3").style.backgroundColor = "#475a7b";
+  document.getElementById("option4").style.backgroundColor = "#475a7b";
+  document.getElementById("option5").style.backgroundColor = "#475a7b";
+}
 
-// function displayHousekeeping() {
-//   hideAllCategory();
-//   showOrHide(housekeepingActivity, true);
-//   document.getElementById("option1").style.backgroundColor = "#202d45";
-// }
+function displayHousekeeping() {
+  hideAllCategory();
+  showOrHide(housekeepingActivity, true);
+  document.getElementById("option1").style.backgroundColor = "#202d45";
+}
 
 // function displayJob() {
 //   hideAllCategory();
