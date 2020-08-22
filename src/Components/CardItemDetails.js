18d4b2e9da298dc83b95c8d982658a644fde691b
Celia Lyu <zexinl@uw.edu>
2020-08-22 15:58:04 -0700

    Published to githubio

--- src/Components/CardItemDetails.js
+++ src/Components/CardItemDetails.js
@@ -14,7 +14,7 @@ export class CardItemDetails extends Component {
         imageSrc,
         imageAlt,
         maskOn: true,
-        activityDuration: 0
+        activityDuration: 0,
       },
     };
   }
@@ -37,8 +37,8 @@ export class CardItemDetails extends Component {
     const { card } = this.state;
 
     if (duration !== card.activityDuration) {
-      this.setState({ card: { ...card, activityDuration: duration } }, 
-        () => this.props.onUpdateCard(this.state.card)
+      this.setState({ card: { ...card, activityDuration: duration } }, () =>
+        this.props.onUpdateCard(this.state.card)
       );
     }
   };
@@ -101,74 +101,3 @@ export class CardItemDetails extends Component {
 }
 
 export default CardItemDetails;
-
-// import React, { Component } from "react";
-// import { hours } from "../Constants/hours";
-
-// export class CardItemDetails extends Component {
-
-//   constructor(props){
-//     super(props);
-
-//     this.state = {
-//       maskOn: true,
-//       activityDuration: hours[0]
-//     };
-//   }
-
-//   onChangeRadioButton = () => {
-//     this.setState({
-//       maskOn: !this.state.maskOn
-//     });
-//   }
-
-//   handleChangeDropdown = (event) => {
-//     this.setState({activityDuration: event.target.value});
-//   }
-
-//   render() {
-//     const { maskOn, activityDuration } = this.state;
-//     const { imageAlt, imageSrc, number, name, onClick} = this.props;
-//     const card = {
-//       name,
-//       number,
-//       maskOn,
-//       activityDuration
-//     };
-
-//     return (
-//       <div className="flex-my-activity-item" role="form">
-//         <div className="row">
-//           <div onClick={() => onClick(card)}>
-//             <div className="close" role="button" aria-label="Close">
-//               ×
-//             </div>
-//           </div>
-//           <div className="col-1 icon">
-//             <img src={imageSrc} alt={imageAlt} className="activityIcon"></img>
-//           </div>
-//           <div className="col">
-//             <h3>{name}</h3>
-//           </div>
-//         </div>
-//         <form>
-//           <div className="card-radio-button">
-//             <label>Wear Mask?</label>
-//             <input type="radio" value="mask-yes" name="yes_no" defaultChecked={true} onClick={this.onChangeRadioButton}/> Yes
-//             <input type="radio" value="mask-no" name="yes_no" onClick={this.onChangeRadioButton}/> No
-//           </div>
-//         </form>
-//         <div className="card-select-box">
-//           <label>Duration?</label>
-//           <select name="duration" value={activityDuration} onChange={this.handleChangeDropdown}>
-//             {hours.map((hour, index) => (
-//               <option key={index} value={index}>{hour}</option>
-//             ))}
-//           </select>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default CardItemDetails;
