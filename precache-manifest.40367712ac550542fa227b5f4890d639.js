748e01d26ce9c3f12972dc71f390d21144aef897
Celia Lyu <zexinl@uw.edu>
2020-08-22 14:38:08 -0700

    Updates

---
+++ precache-manifest.40367712ac550542fa227b5f4890d639.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3533893778d2219939087bb7789ce22d",
+    "url": "/project-01-wendelli-1839314/index.html"
+  },
+  {
+    "revision": "53e58ed7a47246fe3283",
+    "url": "/project-01-wendelli-1839314/static/css/main.ee10ea1f.chunk.css"
+  },
+  {
+    "revision": "3c60fbd38f8a9963c760",
+    "url": "/project-01-wendelli-1839314/static/js/2.9ca91ddf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-01-wendelli-1839314/static/js/2.9ca91ddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e58ed7a47246fe3283",
+    "url": "/project-01-wendelli-1839314/static/js/main.41a09d8c.chunk.js"
+  },
+  {
+    "revision": "4cfe44c6fea96895b518",
+    "url": "/project-01-wendelli-1839314/static/js/runtime-main.37580d2d.js"
+  },
+  {
+    "revision": "350f11701af53907c3e0ba8952489757",
+    "url": "/project-01-wendelli-1839314/static/media/ActivityRiskList.350f1170.csv"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d68ebe5918cc2a2c0ba011db54693ffb.js"
+  "/project-01-wendelli-1839314/precache-manifest.40367712ac550542fa227b5f4890d639.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-01-wendelli-1839314/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
